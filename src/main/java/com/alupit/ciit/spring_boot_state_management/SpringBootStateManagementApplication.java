5a5fa1dc963a5b19859811f5fd3c2419935d947f
Ken Adrian Alupit <alupitkenadrian22@gmail.com>
2024-06-23 12:17:19 +0800

    Initial commit

---
+++ src/main/java/com/alupit/ciit/spring_boot_state_management/SpringBootStateManagementApplication.java
@@ -0,0 +1,13 @@
+package com.alupit.ciit.spring_boot_state_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStateManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStateManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alupit/ciit/spring_boot_state_management/SpringBootStateManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alupit.ciit.spring_boot_state_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStateManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
